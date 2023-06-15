b3fd65a01298421df654922e274f1eadcd8b4f7c
Yanzuc <yanzucchini@gmail.com>
2023-06-15 15:41:27 -0300

    Divisao Pronta

--- Calculadora-divisao.asm
+++ Calculadora-divisao.asm
@@ -6,29 +6,29 @@ result: .word 0
 .globl main 
 main:
     li $v0, 4
-    la $a0, msg
+    la $a0, msg #mostra o conteudo da variavel msg
     syscall
 
-    li $v0, 5
+    li $v0, 5 #le o primeiro numero
     syscall
 
-    move $t0, $v0
+    move $t0, $v0 #armazena o primeiro numero no registrador $t0
 
-    li $v0 5
+    li $v0 5 #le o segundo numero
     syscall
 
-    move $t1, $v0
+    move $t1, $v0 # armazena o segundo numero no registrador $t1
 
-    div $t2, $t0, $t1
-    la $t3, result
-    sw $t2, 0($t3)
+    div $t2, $t0, $t1 # realiza a divisão 
+    la $t3, result 
+    sw $t2, 0($t3) # coloca o resultado da divisão em $t3
 
     li $v0, 4
-    la $a0, resp
+    la $a0, resp #mostra o conteúdo da variavel resp
     syscall
 
     li $v0, 1
-    lw $a0, result
+    lw $a0, result #mostra o contéudo de result
     syscall
 
     li $v0, 10
